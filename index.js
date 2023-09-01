30e1103e3384619379bce60454d50eef08074f0c
saulius <sjsaulius@gmail.com>
2023-09-01 15:45:56 +0300

    add flgas img in js but dont work

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ searchBtn.addEventListener("click", function () {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      console.log(data[0].flags.svg);
+      console.log();
       console.log(data[0].capital[0]);
       console.log(data[0].continents[0]);
       console.log(data[0].population);
       console.log(Object.keys(data[0].currencies)[0]);
       console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
       console.log(Object.values(data[0].languages).toString().split(",").join(", "));
+      result.innerHTMl = `
+      <img src="${data[0].flags.svg}"
+      class="flag-img"
+      `;
     });
 });
