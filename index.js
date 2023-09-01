10bf2fb624f4c54630c65f639292ed46b7f4a116
saulius <sjsaulius@gmail.com>
2023-09-01 15:27:26 +0300

    add fetch info

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ let searchCountry = document.querySelector("#country-name");
 
 searchBtn.addEventListener("click", function () {
   let countryName = searchCountry.value;
+  // Set country name in finalUrl
   let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
   console.log(finalUrl);
+  fetch(finalUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data[0].flags.svg);
+      console.log(data[0].capital[0]);
+      console.log(data[0].continents[0]);
+      console.log(data[0].population);
+      console.log(Object.keys(data[0].currencies)[0]);
+      console.log(data[0].languages);
+    });
 });
