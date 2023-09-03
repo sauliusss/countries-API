981b1bcd835d3b283433756512984d91b1ebdc16
saulius <sjsaulius@gmail.com>
2023-09-03 21:27:20 +0300

    add all info to country card

--- index.js
+++ index.js
@@ -14,15 +14,34 @@ searchBtn.addEventListener("click", function () {
     .then((data) => {
       console.log(data);
       console.log();
-      console.log(data[0].capital[0]);
-      console.log(data[0].continents[0]);
-      console.log(data[0].population);
-      console.log(Object.keys(data[0].currencies)[0]);
-      console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-      console.log(Object.values(data[0].languages).toString().split(",").join(", "));
+      // console.log(data[0].capital[0]);
+      // console.log(data[0].continents[0]);
+      // console.log(data[0].population);
+      // console.log(Object.keys(data[0].currencies)[0]);
+      // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+      // console.log(Object.values(data[0].languages).toString().split(",").join(", "));
       flagBar.innerHTML = `
       <img src="${data[0].flags.svg}"
       class="flag">
+      <div class="country-name">
+      <h2>${data[0].name.common}</h2>
+      </div>
+      <div class="country-info">
+        <div class="capital"><h2>Capital: ${data[0].capital[0]}</h2></div>
+        <div class="continent">
+        <h2>Continent: ${data[0].continents[0]}</h2>
+        </div>
+        <div class="population">
+        <h2>${data[0].population}</h2>
+        </div>
+        <div class="currency">
+        <h2>${Object.keys(data[0].currencies)[0]}</h2>
+        </div>
+        <div class="language">
+        <h2>${Object.values(data[0].languages).toString().split(",").join(", ")}</h2>
+        </div>
+      </div>
+
 `;
     });
 });
